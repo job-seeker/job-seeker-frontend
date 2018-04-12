a3444f5342f37a24140e3b697ff4927ca0953a8a
Ahmed Ali <Ahmed98148@gmail.com>
2018-04-12 14:16:15 -0700

    homepage

---
+++ src/components/homepage/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class HomePage extends React.Component{
+  constructor(props){
+    super(props)
+   }
+  render(){
+    return 
+      <section>
+        <h1> Job Seeker </h1>
+        <p> Get Started </p>
+       </section>
+  }
+}
+
+export default connect(mapStateProps, mapDispatchToProps)(HomePage)
