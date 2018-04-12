5b8d383ffd6af6f83258071a980ba73422fb8969
Ahmed Ali <Ahmed98148@gmail.com>
2018-04-12 14:34:58 -0700

    app scaffold

---
+++ src/components/app/index.js
@@ -0,0 +1,27 @@
+
+import React from 'react'
+import {connect} from 'react-redux'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+
+import Navbar from '../navbar'
+
+class App extends React.Component {
+  componentDidMount(){
+  }
+
+  render(){
+    return (
+      <div className='app'>
+        <BrowserRouter>
+          <div>
+            <Route path='*' component={Navbar} />
+            <Route exact path='/welcome' component={} />
+            <Route exact path='/' component={} />
+            <Route exact path='/' component={} />
+            <Route exact path='/' component={} />
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
