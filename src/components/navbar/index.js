66ba9af1f9322c62292b115d59653300952e4d4d
Ahmed Ali <Ahmed98148@gmail.com>
2018-04-12 14:24:53 -0700

    NavBar scaffold

---
+++ src/components/navbar/index.js
@@ -0,0 +1,29 @@
+
+import React from 'react'
+import {connect} from 'react-redux'
+import {Redirect, Link} from 'react-router-dom'
+
+let NavLink = (props) => (
+  <li className={} >
+    <Link to={}>
+      {props.route}
+    </Link>
+  </li>
+)
+
+class Navbar extends React.Component {
+  constructor(props){
+    super(props)
+    this.handleLogout = this.handleLogout.bind(this)
+  }
+}
+
+let mapStateToProps = (state) => ({
+  
+})
+
+let mapDispatchToProps = (dispatch) => ({
+ 
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
