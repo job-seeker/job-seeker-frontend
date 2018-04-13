bfe908105cab203d6ca3b40267134023db5b3b5b
Ahmed Ali <Ahmed98148@gmail.com>
2018-04-13 10:04:50 -0700

    Test

---
+++ src/__test_/user-auth-actions.test.js
@@ -0,0 +1,22 @@
+import superagent from 'superagent';
+
+const randomNum = max => {
+  return Math.floor(Math.random() * max);
+}
+
+const mockUser = {
+  username: `usertest${randomNum(5000)}`,
+  email: `emailtest${randomNum(5000)}@email.com`,
+  password: 'a1b2c3d4e5'
+}
+
+describe('Auth Actions', () => {
+  let tempUser;
+
+  test('should return a TOKEN_SET action', () => {
+    let action = tokenSet({ token: '12345' });
+    expect(action.type).toEqual('TOKEN_SET');
+    expect(action.payload).toBeTruthy();
+    expect(action.payload.token).toBe('12345');
+  });
+});
