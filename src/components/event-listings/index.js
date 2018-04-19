77dd2b56a8fdb204bbaec76060abfdbdc360588f
Melanie Cohen <melaniebcohen@gmail.com>
2018-04-19 10:10:29 -0700

    link to job view

--- src/components/event-listings/index.js
+++ src/components/event-listings/index.js
@@ -8,6 +8,7 @@ import AddIcon from 'material-ui/svg-icons/content/add-circle';
 import ListingTable from '../listing-table';
 import EventModal from '../event-modal';
 import { eventCreateRequest } from '../../actions/event-actions';
+import { amber800 } from 'material-ui/styles/colors';
 
 class EventListings extends React.Component {
   constructor(props) {
@@ -31,14 +32,28 @@ class EventListings extends React.Component {
     return (
       <section className='dashboard-content'>
         <DashboardNav />
-        <ListingTable />
 
-        <IconButton onClick={this.handleModalOpen}>
-          <AddIcon />
-        </IconButton>
+        <ListingTable 
+          style={{ 'padding-bottom': '20px' }}
+          header='All Events'
+          column1='Event'
+          column2='Company'
+          column3='Event Date'
+          column4='Date Added'
+          profile={this.props.profile}
+        />
+
+        {/* <IconButton 
+          style={{ marginLeft: 12 }}
+          iconStyle={{ height: 35, width: 35 }}
+          className='add-icon' 
+          onClick={this.handleModalOpen}>
+          <AddIcon color={amber800} />
+        </IconButton> */}
 
         <EventModal 
           open={this.state.modalOpen}
+          profile={this.props.profile}
           onComplete={this.props.eventCreate}
           modalClose={this.handleModalClose}
         />
@@ -47,8 +62,12 @@ class EventListings extends React.Component {
   }
 }
 
+let mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
 let mapDispatchToProps = (dispatch) => ({
   eventCreate: event => dispatch(eventCreateRequest(event)),
 });
 
-export default connect(null, mapDispatchToProps)(EventListings);
+export default connect(mapStateToProps, mapDispatchToProps)(EventListings);
--- src/components/listing-table/index.js
+++ src/components/listing-table/index.js
@@ -4,14 +4,7 @@ import { Link } from 'react-router-dom';
 import './_listing-table.scss';
 import CompanyView from '../company-view';
 
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
 import { Subheader } from 'material-ui';
 
 export default class ListingTable extends Component {
@@ -43,6 +36,38 @@ export default class ListingTable extends Component {
                     <TableRowColumn>{company.cityState}</TableRowColumn>
                     <TableRowColumn>{new Date(company.created).toDateString()}</TableRowColumn>
                   </TableRow>
+                ))
+                : undefined
+              : undefined
+            }
+
+            {this.props.profile 
+              ? this.props.header === 'All Jobs'
+                ? this.props.profile.companies.map((company) => (
+                  company.jobPosting.map((job) => 
+                    <TableRow key={job._id}>
+                      <TableRowColumn><Link to={'/company/'+company._id}>{job.title}</Link></TableRowColumn>
+                      <TableRowColumn>{company.companyName}</TableRowColumn>
+                      <TableRowColumn>{job.status}</TableRowColumn>
+                      <TableRowColumn>{new Date(job.created).toDateString()}</TableRowColumn>
+                    </TableRow>
+                  )
+                ))
+                : undefined
+              : undefined
+            }
+
+            {this.props.profile 
+              ? this.props.header === 'All Events'
+                ? this.props.profile.companies.map((company) => (
+                  company.events.map((event) => 
+                    <TableRow key={event._id}>
+                      <TableRowColumn><Link to={'/company/'+company._id}>{event.eventTitle}</Link></TableRowColumn>
+                      <TableRowColumn>{company.companyName}</TableRowColumn>
+                      <TableRowColumn>{new Date(event.eventDate).toDateString()}</TableRowColumn>
+                      <TableRowColumn>{new Date(event.created).toDateString()}</TableRowColumn>
+                    </TableRow>
+                  )
                 ))
                 : undefined
               : undefined
