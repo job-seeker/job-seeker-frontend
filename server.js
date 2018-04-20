c68efdd07a942d7ab40b92a66f5ec38014324e5e
Melanie Cohen <melaniebcohen@gmail.com>
2018-04-19 22:22:48 -0700

    favicon

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const express = require('express');
+const favicon = require('serve-favicon');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(favicon(`${__dirname}/favicon.ico`));
 app.use(express.static(`${__dirname}/build`));
 
 app.listen(PORT, () => {
